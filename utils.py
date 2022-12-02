b50178f89f9c3ef8db00e8be644ea6aea022c3d0
4pius <116029941+4pius@users.noreply.github.com>
2022-12-02 13:19:10 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import logging
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
+from umongo import Instance, Document, fields
 from motor.motor_asyncio import AsyncIOMotorClient
 from info import AUTH_CHANNEL, API_KEY
 import asyncio
